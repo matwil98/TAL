417a598d0695130a92a3fcba8707185a0edb47dc
Mateusz <mateusz.wilusz.mf14@gmail.com>
2021-05-19 00:21:41 +0200

    Creating method which returns ArrayList of taken elements

--- src/pl/wat/edu/tal/Knapsack.java
+++ src/pl/wat/edu/tal/Knapsack.java
@@ -7,8 +7,7 @@ public class Knapsack {
     ArrayList<Integer> objects = new ArrayList<>();
     ArrayList<Float> profits;
     ArrayList<Float> weights;
-    ArrayList<Integer> weightsI;
-    ArrayList<Integer> valuesI;
+
     int maxWeights;
     String sumProfits_sumWeights;
 
@@ -92,30 +91,50 @@ public class Knapsack {
         return results_objects;
     }
 
-//    public ArrayList<Integer> solveKnapsackDP(int numOfItems, int capacity) {
-//        ArrayList<Integer> resultsObjects = new ArrayList<>();
-//        int knapsackTableSave[][] = new int[weightsI.size() + 1][valuesI.size() + 1];
-//        for (int j = 0; j < knapsackTableSave.length; j++) {
-//            knapsackTableSave[0][j] = 0;
-//        }
-//
-//        for (int i = 1; i <= numOfItems; i++) {
-//            for (int w = 1; w <= capacity; w++) {
-//                if (numOfItems <= 0) {
-//                    System.out.println("Nie ma żadnych elementów do zapakowania");
-//                } else if (weightsI.get(i) > w) {
-//                    knapsackTableSave[i][w] = knapsackTableSave[i - 1][w];
-//                } else {
-//                    knapsackTableSave[i][w] = Math.max(knapsackTableSave[i - 1][w],
-//                            knapsackTableSave[i - 1][w - weightsI.get(i)] + valuesI.get(i));
-//
-//                }
-//            }
-//            return 0;
-//        }
-//
-//    }
+    public ArrayList<Integer> solveKnapsackDP(ArrayList<Integer> weightsI, ArrayList<Integer> valuesI, int numOfItems, int capacity) {
 
+
+        ArrayList<Integer> takenElements = new ArrayList<>();
+        ArrayList<Integer> timesWeightAndValue = new ArrayList<Integer>();
+
+        int[][] knapsackTableSave = new int[numOfItems + 1][capacity + 1];
+        for (int j = 0; j < knapsackTableSave.length; j++) {
+            knapsackTableSave[0][j] = 0;
+        }
+
+        for (int i = 1; i <= numOfItems; i++) {
+            for (int w = 1; w <= capacity; w++) {
+
+                if (weightsI.get(i - 1) > w) {
+                    knapsackTableSave[i][w] = knapsackTableSave[i - 1][w];
+                } else {
+                    knapsackTableSave[i][w] = Math.max(knapsackTableSave[i - 1][w],
+                            knapsackTableSave[i - 1][w - weightsI.get(i - 1)] + valuesI.get(i - 1));
+
+                }
+            }
+
+        }
+
+        int currentKnapsackCapacity = capacity;
+        for (int i = numOfItems; i >= 1; i--) {
+            if (knapsackTableSave[i][currentKnapsackCapacity] > knapsackTableSave[i - 1][currentKnapsackCapacity]) {
+                takenElements.add(i);
+                timesWeightAndValue.add(valuesI.get(i - 1));
+                currentKnapsackCapacity -= weightsI.get(i - 1);
+                System.out.println(currentKnapsackCapacity);
+
+            } else {
+                System.out.println("Przedmiot nr " + i + " nie jest pakowany do koszyka");
+            }
+        }
+
+        System.out.println("Wartość zabranych przedmiotów: " + countElementsOfArray(timesWeightAndValue));
+        System.out.println("Zebrane elementy");
+        System.out.println(takenElements);
+        return takenElements;
+
+    }
 
 
     public int getIndiceMax(ArrayList<Float> fraction) {
@@ -129,6 +148,22 @@ public class Knapsack {
         }
         fraction.set(maxIndice, (float) -1);
         return maxIndice;
+    }
+
+    public ArrayList<Integer> convertTableToArrayList(int[] t) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < t.length; i++) {
+            arrayList.add(t[i]);
+        }
+        return arrayList;
+    }
+
+    public int countElementsOfArray(ArrayList<Integer> arrayList) {
+        int amount = 0;
+        for (Integer i : arrayList) {
+            amount += i;
+        }
+        return amount;
     }
 
 }
